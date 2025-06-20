2a6503859ad9baccf04822763678c758fa271616
LuisEduardoRomeroOlmos <luiseduardoromeroolmos@aol.com>
2025-06-19 19:05:45 -0600

    cambios

--- app3.py
+++ app3.py
@@ -33,114 +33,113 @@ def cargar_modelo():
 modelo = cargar_modelo()
 
 @st.cache_data
-def cargar_nombres_clases_oficial_es():
+def cargar_nombres_clases_oficial():
     return {
-        0: "Prímula rosa",
-        1: "Orquídea de hojas duras",
-        2: "Campanas de Canterbury",
-        3: "Guisante dulce",
-        4: "Caléndula inglesa",
-        5: "Lirio tigre",
-        6: "Orquídea luna",
-        7: "Ave del paraíso",
-        8: "Acónito",
-        9: "Cardo globo",
-        10: "Boca de dragón",
-        11: "Farfara",
-        12: "Protea rey",
-        13: "Cardo lanzador",
-        14: "Iris amarillo",
-        15: "Flor globo",
-        16: "Echinácea púrpura",
-        17: "Lirio peruano",
-        18: "Flor globo (balloon flower)",
-        19: "Lirio arum gigante blanco",
-        20: "Lirio de fuego",
-        21: "Flor alfiler",
-        22: "Fritilaria",
-        23: "Jengibre rojo",
-        24: "Jacinto de uva",
-        25: "Amapola del campo",
-        26: "Plumas del príncipe de Gales",
-        27: "Genciana sin tallo",
-        28: "Alcachofa",
-        29: "Dianthus barbatus (Clavel del poeta)",
-        30: "Clavel",
-        31: "Flores de jardín (Phlox)",
-        32: "Amor en la niebla",
-        33: "Aster mexicano",
-        34: "Eryngium alpino",
-        35: "Orquídea cattleya",
-        36: "Flor del cabo",
-        37: "Astrantia mayor",
-        38: "Tulipán siamés",
-        39: "Rosa de Cuaresma",
-        40: "Margarita barbeton",
-        41: "Narciso",
-        42: "Gladiolo",
-        43: "Flor de Pascua",
-        44: "Bolero azul profundo",
-        45: "Alhelí",
-        46: "Caléndula",
-        47: "Ranúnculo",
-        48: "Margarita común",
-        49: "Diente de león común",
-        50: "Petunia",
-        51: "Pensamiento silvestre",
-        52: "Prímula",
-        53: "Girasol",
-        54: "Geranio",
-        55: "Obispo de Llandaff",
-        56: "Gaura",
-        57: "Geranio",
-        58: "Dalia naranja",
-        59: "Dalia rosa-amarilla",
-        60: "Cautleya spicata",
-        61: "Anémona japonesa",
-        62: "Susana de ojos negros",
-        63: "Arbusto plateado",
-        64: "Amapola californiana",
-        65: "Osteospermum",
-        66: "Crocus de primavera",
-        67: "Iris",
-        68: "Anémona",
-        69: "Amapola arbórea",
-        70: "Gazania",
-        71: "Azalea",
-        72: "Nenúfar",
-        73: "Rosa",
-        74: "Manzana espinosa",
-        75: "Gloria de la mañana",
-        76: "Flor de la pasión",
-        77: "Loto",
-        78: "Lirio sapo",
-        79: "Anturio",
-        80: "Frangipani",
-        81: "Clemátide",
-        82: "Hibisco",
-        83: "Aguileña",
-        84: "Rosa del desierto",
-        85: "Malva arbórea",
-        86: "Magnolia",
-        87: "Ciclamen",
-        88: "Berro de agua",
-        89: "Caña de lirio",
-        90: "Amarilis",
-        91: "Bálsamo de abeja",
-        92: "Flor globo",
-        93: "Gallardia",
-        94: "Dedalera",
-        95: "Buganvilla",
-        96: "Camelia",
-        97: "Malva",
-        98: "Petunia mexicana",
-        99: "Bromelia",
-        100: "Gallardia",
-        101: "Enredadera trompeta"
+        0: "prímula rosada",               # carpeta 0
+        1: "orquídea de hoja dura",        # carpeta 1
+        2: "boca de dragón",               # carpeta 10
+        3: "enredadera trompeta",          # carpeta 100
+        4: "lirio mora",                   # carpeta 101
+        5: "pata de caballo",              # carpeta 11
+        6: "protea real",                  # carpeta 12
+        7: "cardo lanudo",                 # carpeta 13
+        8: "iris amarillo",                # carpeta 14
+        9: "flor globo",                  # carpeta 15
+        10: "cono púrpura",                # carpeta 16
+        11: "lirio peruano",               # carpeta 17
+        12: "flor globo",                  # carpeta 18
+        13: "lirio arum blanco gigante",  # carpeta 19
+        14: "campanillas de Canterbury",  # carpeta 2
+        15: "lirio de fuego",              # carpeta 20
+        16: "flor de cojín",               # carpeta 21
+        17: "fritilaria",                  # carpeta 22
+        18: "jengibre rojo",               # carpeta 23
+        19: "jacinto de uva",              # carpeta 24
+        20: "amapola silvestre",           # carpeta 25
+        21: "pluma del príncipe de Gales",# carpeta 26
+        22: "genciana sin tallo",          # carpeta 27
+        23: "alcachofa",                   # carpeta 28
+        24: "clavel del poeta",            # carpeta 29
+        25: "guisante de olor",            # carpeta 3
+        26: "clavel",                     # carpeta 30
+        27: "flores de jardín",            # carpeta 31
+        28: "amor en la niebla",           # carpeta 32
+        29: "áster mexicano",              # carpeta 33
+        30: "cardo azul alpino",           # carpeta 34
+        31: "cattleya labios rubí",        # carpeta 35
+        32: "flor del Cabo",               # carpeta 36
+        33: "gran astrancia",              # carpeta 37
+        34: "tulipán siamés",              # carpeta 38
+        35: "rosa de cuaresma",            # carpeta 39
+        36: "caléndula inglesa",           # carpeta 4
+        37: "barbetón margarita",          # carpeta 40
+        38: "narciso",                    # carpeta 41
+        39: "lirio espada",                # carpeta 42
+        40: "flor de pascua",              # carpeta 43
+        41: "bolero azul profundo",        # carpeta 44
+        42: "flor de pared",               # carpeta 45
+        43: "caléndula",                   # carpeta 46
+        44: "botón de oro",                # carpeta 47
+        45: "margarita común",             # carpeta 48
+        46: "diente de león",              # carpeta 49
+        47: "lirio tigre",                 # carpeta 5
+        48: "petunia",                    # carpeta 50
+        49: "pensamiento silvestre",      # carpeta 51
+        50: "prímula",                    # carpeta 52
+        51: "girasol",                    # carpeta 53
+        52: "pelargonio",                 # carpeta 54
+        53: "dalia obispo de llandaff",   # carpeta 55
+        54: "gaura",                     # carpeta 56
+        55: "geranio",                   # carpeta 57
+        56: "dalia naranja",              # carpeta 58
+        57: "dalia rosa y amarilla",      # carpeta 59
+        58: "orquídea lunar",             # carpeta 6
+        59: "cautleya espinosa",          # carpeta 60
+        60: "anémona japonesa",            # carpeta 61
+        61: "susana de ojos negros",      # carpeta 62
+        62: "arbusto plateado",           # carpeta 63
+        63: "amapola californiana",       # carpeta 64
+        64: "osteospermo",                # carpeta 65
+        65: "azafrán de primavera",       # carpeta 66
+        66: "iris barbado",               # carpeta 67
+        67: "anémona",                   # carpeta 68
+        68: "amapola arbórea",            # carpeta 69
+        69: "ave del paraíso",            # carpeta 7
+        70: "gazanias",                  # carpeta 70
+        71: "azalea",                    # carpeta 71
+        72: "nenúfar",                   # carpeta 72
+        73: "rosa",                      # carpeta 73
+        74: "manzana espinosa",           # carpeta 74
+        75: "gloria de la mañana",        # carpeta 75
+        76: "pasiflora",                  # carpeta 76
+        77: "loto",                      # carpeta 77
+        78: "lirio sapo",                # carpeta 78
+        79: "anturio",                   # carpeta 79
+        80: "casco de monje",             # carpeta 8
+        81: "frangipani",                # carpeta 80
+        82: "clemátide",                 # carpeta 81
+        83: "hibisco",                   # carpeta 82
+        84: "aguileña",                  # carpeta 83
+        85: "rosa del desierto",          # carpeta 84
+        86: "malva arbórea",              # carpeta 85
+        87: "magnolia",                 # carpeta 86
+        88: "ciclamen",                 # carpeta 87
+        89: "berro",                    # carpeta 88
+        90: "canna índica",             # carpeta 89
+        91: "cardo globo",               # carpeta 9
+        92: "hippeastrum",              # carpeta 90
+        93: "bergamota",                # carpeta 91
+        94: "musgo de bola",            # carpeta 92
+        95: "dedalera",                 # carpeta 93
+        96: "bugambilia",               # carpeta 94
+        97: "camelia",                 # carpeta 95
+        98: "malva",                   # carpeta 96
+        99: "petunia mexicana",         # carpeta 97
+        100: "bromelia",               # carpeta 98
+        101: "gallardía"               # carpeta 99
     }
 
-
-nombres_clases = cargar_nombres_clases_oficial_es()
+nombres_clases = cargar_nombres_clases_oficial()
 
 def obtener_info_wikipedia(flor_nombre):
     try:
