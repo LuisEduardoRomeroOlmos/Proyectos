356a89b4186fb08c5f13cae7f412968a78f79269
Luis Eduardo Romero Olmos <luiseduardoromeroolmos@aol.com>
2025-07-14 02:30:06 -0600

    Update gradcam.py

--- utils/gradcam.py
+++ utils/gradcam.py
@@ -3,35 +3,20 @@ from PIL import Image
 import matplotlib.cm as cm
 import tensorflow as tf
 
-def make_gradcam_heatmap(img_array, model):
-    """
-    img_array: imagen preprocesada con shape (1, H, W, 3)
-    model: modelo con 2 salidas: [conv_outputs, predictions]
-    """
+def make_gradcam_heatmap(img_batch, model):
+    # img_batch: shape (1, H, W, 3)
+    # model: devuelve [conv_outputs, predictions]
     with tf.GradientTape() as tape:
-        conv_outputs, predictions = model(img_array, training=False)
-        
-        # Si tu salida es una sola neurona (sigmoide), toma índice 0
-        pred_index = 0
-        
-        # Para multi-clase sería: pred_index = tf.argmax(predictions[0])
-        
-        class_channel = predictions[:, pred_index]
-
-    grads = tape.gradient(class_channel, conv_outputs)  # Gradientes de la salida con respecto a la última conv
-    pooled_grads = tf.reduce_mean(grads, axis=(0, 1, 2))  # Promedio de gradientes por canal
-
-    conv_outputs = conv_outputs[0]  # Quitar batch dimension
-    
-    # Ponderar cada canal por su gradiente promedio y sumar
-    heatmap = tf.reduce_sum(tf.multiply(conv_outputs, pooled_grads), axis=-1)
-    
-    # Normalizar y aplicar ReLU para quitar negativos
-    heatmap = tf.maximum(heatmap, 0)
-    max_val = tf.reduce_max(heatmap)
-    heatmap = heatmap / (max_val + 1e-10)
-    
-    return heatmap.numpy()
+        conv_outputs, preds = model(img_batch, training=False)
+        # Si tu salida es sigmoide (1 neurona), siempre tomas índice 0:
+        class_channel = preds[:, 0]
+    grads = tape.gradient(class_channel, conv_outputs)
+    pooled_grads = tf.reduce_mean(grads, axis=(0,1,2))
+    conv_outputs = conv_outputs[0]
+    # Suma ponderada de los mapas:
+    heatmap = tf.reduce_sum(conv_outputs * pooled_grads, axis=-1)
+    heatmap = tf.maximum(heatmap, 0) / (tf.reduce_max(heatmap) + 1e-10)
+    return heatmap.numpy(
 
 def save_and_display_gradcam(img, heatmap, alpha=0.4):
     img = img.resize((300, 300))
