34cb2b9c07d9f7528c6bee5ed43dadec1d7aa8df
Luis Eduardo Romero Olmos <luiseduardoromeroolmos@aol.com>
2025-07-14 00:45:40 -0600

    Update app6.py

--- app6.py
+++ app6.py
@@ -6,7 +6,7 @@ from utils.gradcam import make_gradcam_heatmap, save_and_display_gradcam
 from utils.preprocessing import preprocess_image
 from tensorflow.keras.models import Model
 
-# Carga del modelo
+# Carga del modelo completo
 model = tf.keras.models.load_model('Modelos/Clasificacion_melanoma_V1_P2.keras')
 
 st.set_page_config(page_title="Detección de Melanoma", layout="centered")
@@ -16,33 +16,32 @@ uploaded_file = st.file_uploader("Sube una imagen de la piel", type=["jpg", "jpe
 
 if uploaded_file is not None:
     image = Image.open(uploaded_file).convert("RGB")
-    st.image(image, caption="Imagen subida", use_container_width=True)  # usa use_container_width
+    st.image(image, caption="Imagen subida", use_container_width=True)
 
-    # Preprocesar imagen
+    # Preprocesamiento
     img_array = preprocess_image(image)
 
-    # Predicción normal con modelo completo
+    # Predicción
     pred = model.predict(np.expand_dims(img_array, axis=0))[0][0]
     label = "Melanoma" if pred >= 0.5 else "No Melanoma"
     conf = pred if pred >= 0.5 else 1 - pred
     st.write(f"**Predicción:** {label} ({conf*100:.2f}%)")
 
-    # Preparar modelo para Grad-CAM
-    base_model = model.layers[0]
+    # Grad-CAM
+    base_model = model.layers[0]  # EfficientNetB3
     last_conv_layer = base_model.get_layer("top_conv")
 
-
+    # Crear modelo para Grad-CAM: usa input y output del modelo completo
     gradcam_model = Model(
-    inputs=base_model.input,        # entrada del base_model (EfficientNetB3)
-    outputs=[last_conv_layer.output, model.output]
+        inputs=model.input,
+        outputs=[last_conv_layer.output, model.output]
     )
 
-
-    # Generar heatmap con gradcam_model
+    # Generar heatmap
     heatmap = make_gradcam_heatmap(
         np.expand_dims(img_array, axis=0),
-        gradcam_model,
-        last_conv_layer_name=None  # porque ya se define en las salidas
+        gradcam_model
     )
+
     gradcam_image = save_and_display_gradcam(image, heatmap)
     st.image(gradcam_image, caption="Grad-CAM", use_container_width=True)
