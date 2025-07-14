bdff31212dadf491472ee87b56d3693a54d220a6
Luis Eduardo Romero Olmos <luiseduardoromeroolmos@aol.com>
2025-07-14 02:35:17 -0600

    Update gradcam.py

--- utils/gradcam.py
+++ utils/gradcam.py
@@ -4,18 +4,38 @@ import matplotlib.cm as cm
 import tensorflow as tf
 
 def make_gradcam_heatmap(img_batch, model):
-    # img_batch: shape (1, H, W, 3)
-    # model: devuelve [conv_outputs, predictions]
+    """
+    img_batch: batch de 1 imagen preprocesada con shape (1, H, W, 3)
+    model: modelo funcional con dos salidas [conv_outputs, preds]
+    """
     with tf.GradientTape() as tape:
+        # Ejecutamos el modelo para obtener activaciones y predicciones
         conv_outputs, preds = model(img_batch, training=False)
-        # Si tu salida es sigmoide (1 neurona), siempre tomas índice 0:
+
+        # Nos aseguramos de que tape observe conv_outputs
+        tape.watch(conv_outputs)
+
+        # Como es binaria, tomamos la neurona 0
         class_channel = preds[:, 0]
+
+    # Gradiente de la clase con respecto a conv_outputs
     grads = tape.gradient(class_channel, conv_outputs)
-    pooled_grads = tf.reduce_mean(grads, axis=(0,1,2))
+
+    # Ahora sí gradientes no serán None
+    # Promediamos sobre el batch y la altura/anchura
+    pooled_grads = tf.reduce_mean(grads, axis=(0, 1, 2))
+
+    # Quitamos la dim de batch de conv_outputs
     conv_outputs = conv_outputs[0]
-    # Suma ponderada de los mapas:
+
+    # Weighted sum: conv_outputs * pooled_grads, sumando por canal
     heatmap = tf.reduce_sum(conv_outputs * pooled_grads, axis=-1)
-    heatmap = tf.maximum(heatmap, 0) / (tf.reduce_max(heatmap) + 1e-10)
+
+    # ReLU + normalización
+    heatmap = tf.maximum(heatmap, 0)
+    max_val = tf.reduce_max(heatmap)
+    heatmap = heatmap / (max_val + 1e-10)
+
     return heatmap.numpy()
 
 def save_and_display_gradcam(img, heatmap, alpha=0.4):
@@ -23,11 +43,11 @@ def save_and_display_gradcam(img, heatmap, alpha=0.4):
     heatmap = np.uint8(255 * heatmap)
 
     colormap = cm.get_cmap('jet')
-    heatmap_color = colormap(heatmap / 255.0)[:, :, :3]  # solo RGB, sin canal alpha
+    heatmap_color = colormap(heatmap / 255.0)[:, :, :3]
     heatmap_color = np.uint8(heatmap_color * 255)
 
-    # Superponer heatmap sobre imagen original
-    superimposed_img = np.array(img) * (1 - alpha) + heatmap_color * alpha
-    superimposed_img = np.uint8(superimposed_img)
+    superimposed = np.array(img) * (1 - alpha) + heatmap_color * alpha
+    superimposed = np.uint8(superimposed)
 
-    return Image.fromarray(superimposed_img)
+    return Image.fromarray(superimposed)
+
